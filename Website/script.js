6fdad20c92ffc0c35380f4acc6fbcd2a1fd32372
UtechP-40 <pradeep2420pradeep@gmail.com>
2024-08-21 00:17:21 +0530

    Web Structure

---
+++ Website/script.js
@@ -0,0 +1,7 @@
+let loader = document.querySelector("#preloader");
+
+window.addEventListener("load", function() {
+    setTimeout(function() {
+        loader.style.display = "none";
+    }, 2500); 
+});
