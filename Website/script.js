2a0cd3ab83eb1f094df3aa6871a00ec560b9587d
UtechP-40 <pradeep2420pradeep@gmail.com>
2024-08-21 04:51:14 +0530

    Navbar Changed and logo added

--- Website/script.js
+++ Website/script.js
@@ -3,5 +3,5 @@ let loader = document.querySelector("#preloader");
 window.addEventListener("load", function() {
     setTimeout(function() {
         loader.style.display = "none";
-    }, 500); 
+    }, 3500); 
 });
